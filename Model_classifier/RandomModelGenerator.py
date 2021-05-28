25236f021003a8f4ad407900a404bd954e2ccf07
PawelKopciewicz <48591214+PawelKopciewicz@users.noreply.github.com>
2021-05-28 19:47:38 +0200

    Dictionary of models added

--- Model_classifier/RandomModelGenerator.py
+++ Model_classifier/RandomModelGenerator.py
@@ -3,42 +3,36 @@ import numpy as np
 import math
 from random import random
 
-def Gauss( x, A, u, sigma ):
-    return  A * np.exp(-np.power(x - u, 2) / (2 * np.power(sigma, 2)))
+def class_option(option):
+    return {
+        0 : 'Gauss',
+        1 : 'Gauss+Gauss',
+        2 : 'Gauss+Exp',
+        3 : 'Exp',
+    }[option]
 
-def generate_data(X_data, Y_data, rate):
+def Gauss(x, A, u, sigma ):
+    for i in range(len(x)):
+        x[i] = A * np.exp(-np.power(x[i] - u, 2) / (2 * np.power(sigma, 2)))
+    return x
+def Exp(x, A, b):
+    for i in range(len(x)):
+        x[i] = A * math.exp(-b * x[i])
+    return x
 
-    X_data = np.array(X_data, dtype = np.float32)
-    Y_data = np.array(Y_data, dtype = np.longlong)
+def data_generator(X, option_model):
+    return {
+        'Gauss' : Gauss(X, 1, 3 + 4*random(), 0.2 + random()),
+        'Gauss+Gauss' : Gauss(X, 1, 3 + random(), 0.2 + random()) + Gauss(X, 1, 6 + random(), 0.2 + random()),
+        'Gauss+Exp' : Gauss(X, 1, 3 + 4*random(), 0.2 + random() ) + Exp(X, 1, 0.5 + random() ),
+        'Exp' : Exp(X, 1, 0.5 + random() ),
+    }[option_model]
+
+def generate_data(X_data, option):
 
     # Gauss, class 0
-    for i in range(500):
-        u = 4.5 + random()
-        std = 0.5 + 0.5*random()
-        for j in range(20):
-            X_data[i][j] = Gauss(X_data[i][j], 1, u, std )
-        Y_data[i] = 0
-    #Gauss + Exp, class 2
-    for i in range(500,1000):
-        u = 4.5 + random()
-        std = 0.5 + 0.5*random()
-        b = 0.4 + 0.2 * random()
-        for j in range(20):
-            X_data[i][j] = Gauss(X_data[i][j], 1, u, std ) + math.exp(-b * X_data[i][j])
-        Y_data[i] = 2
-    #Exp, class 3
-    for i in range(1000,1500):
-        b = 0.4 + 0.2 * random()
-        for j in range(20):
-            X_data[i][j] = math.exp(-b * X_data[i][j])
-        Y_data[i] = 2
-    #Gauss + Gauss, clas 1
-    for i in range(1500,2000):
-        u1 = 3.5 + random()
-        std1 = 0.5 + 0.5*random()
-        u2 = 6.5 + random()
-        std2 = 0.5 + 0.5*random()
-        for j in range(20):
-            X_data[i][j] = Gauss(X_data[i][j], 1, u1, std1 ) + Gauss(X_data[i][j], 1, u2, std2 )
-        Y_data[i] = 1
-    return X_data, Y_data
+    option_model = class_option(option)
+    X_data = data_generator(X_data, option_model)
+
+    return X_data
+

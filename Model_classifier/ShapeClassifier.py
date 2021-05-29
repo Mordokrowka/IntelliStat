7716643b6d4cab23294633eac81214f9dd20a3d1
PawelKopciewicz <48591214+PawelKopciewicz@users.noreply.github.com>
2021-05-29 22:56:08 +0200

    Update ShapeClassifier.py

--- Model_classifier/ShapeClassifier.py
+++ Model_classifier/ShapeClassifier.py
@@ -13,22 +13,22 @@ def Gauss( x, A, u, sigma ):
 
 
 def main():
-    epoch = 50
-    train_samples = 400
+    epoch = 60
+    train_samples = 1200
     test_samples = 200
-    classes = 4
-    EvolutionalNN = ENN_classifier(20, 20, 10, 5, classes)
+    classes = 6
+    EvolutionalNN = ENN_classifier(40, 40, 20, 10, classes)
 
 
-    X_data = [[X/2 for X in range(20)] for it in range(classes*(train_samples + test_samples))]
+    X_data = [[X/4 for X in range(40)] for it in range(classes*(train_samples + test_samples))]
     Y_data = [ 0 for X in X_data]
     X_data = np.array(X_data, dtype = np.float32)
     Y_data = np.array(Y_data, dtype = np.longlong)
 
     for c in range(classes):
         for i in range(train_samples + test_samples):
-            X_data[i] = generate_data(X_data[i + c*(train_samples + test_samples)], c)
-            Y_data[i] = c
+            X_data[i + c*(train_samples + test_samples)] = generate_data(X_data[i + c*(train_samples + test_samples)], c)
+            Y_data[i + c*(train_samples + test_samples)] = c
 
     indices = np.arange(X_data.shape[0])
     np.random.shuffle(indices)
@@ -45,26 +45,30 @@ def main():
     X_NN = torch.tensor(X_test)
     Y_NN = EvolutionalNN.model(X_NN)
     Y_NN = Y_NN.detach().numpy()
-
+    #print(X_NN, Y_NN)
     #idx = np.zeros([classes * (train_samples), classes])
-    idx_gauss = []
+    idx_gge = []
     accuraccy = 0
     for i in range(len(Y_NN)):
-        if Y_test[i] == 0:
-            idx_gauss.append(i)
+        if Y_test[i] == 3:
+            idx_gge.append(i)
         #print(Y_test[i], Y_NN[i, Y_test[i]])
-        if round(Y_NN[i,Y_test[i]]) == 1:
+        if Y_NN[i,Y_test[i]] == max(Y_NN[i]):
             accuraccy = accuraccy + 1
     accuraccy = accuraccy / (classes * test_samples)
     print("Reached accuraccy: ",accuraccy)
 
+    #for it in range(len(Y_NN)):
+    #    print(Y_NN[it], Y_test[it])
     fig, ax = plt.subplots(2,2)
     fig.set_size_inches(8, 8)
-    n, bins, patches = ax[0, 0].hist(Y_NN[idx_gauss,0], 100, alpha=0.5, range = [0,1], color = 'red', label='Gauss')
-    #n, bins, patches = ax[0, 0].hist(Y_NN[gaussgauss_i, 0], 100, alpha=0.5, range = [0,1],color='blue', label='Gauss + Gauss')
-    #n, bins, patches = ax[0, 0].hist(Y_NN[gaussexp_i, 0], 100, alpha=0.5, range = [0,1],color='green', label='Gauss + Exp')
-    #n, bins, patches = ax[0, 0].hist(Y_NN[exp_i, 0], 100, alpha=0.5, range=[0, 1], color='yellow', label='Exp')
-    ax[0, 0].set_xlabel('Class A (Gauss)')
+    n, bins, patches = ax[0, 0].hist(Y_NN[idx_gge, 0], 100, alpha=0.5, range=[0, 1], label='Gauss')
+    n, bins, patches = ax[0, 0].hist(Y_NN[idx_gge, 1], 100, alpha=0.5, range=[0, 1], label='Gauss+Gauss')
+    n, bins, patches = ax[0, 0].hist(Y_NN[idx_gge, 2], 100, alpha=0.5, range = [0,1], label='Gauss+Gauss+Gauss')
+    n, bins, patches = ax[0, 0].hist(Y_NN[idx_gge, 3], 100, alpha=0.5, range = [0,1], label='Gauss+Gauss+Exp')
+    n, bins, patches = ax[0, 0].hist(Y_NN[idx_gge, 4], 100, alpha=0.5, range = [0,1], label='Gauss+Exp')
+    n, bins, patches = ax[0, 0].hist(Y_NN[idx_gge, 5], 100, alpha=0.5, range=[0, 1],  label='Exp')
+    ax[0, 0].set_xlabel('Class A (Gauss+Gauss+Exp)')
     ax[0, 0].set_ylabel('Number of counts')
     ax[0, 0].legend(loc='upper right')
     ax[0, 0].set_title('Class affiliation probability')
@@ -74,9 +78,16 @@ def main():
     ax[1, 0].set_ylabel('Loss function')
     ax[1, 0].set_yscale('log')
 
-    #ax[0,0].plot(np.linspace(0, 10, vis_len, endpoint=False), In_data[1], 'k-', label="Gauss no. 1")
-    #ax[0,0].plot(np.linspace(0, 10, vis_len, endpoint=False), In_data[2], 'r-', label="Gauss no. 2")
-    #ax[0,0].plot(np.linspace(0, 10, vis_len, endpoint=False), In_data[3], 'b-', label="Gauss no. 3")
+    X_plot = [X / 40 for X in range(400)]
+    X_plot = np.array(X_plot, dtype = np.float32)
+    X_plot = generate_data(X_plot, 3)
+    ax[0, 1].plot(np.linspace(0, 10, 400, endpoint=False), X_plot, 'r-', label="GGE(x)")
+    ax[0, 1].set_xlabel('X argument')
+    ax[0, 1].set_ylabel('GGE(x)')
+    ax[0, 1].legend(loc='upper right')
+    ax[0, 1].set_title('Exemplary training GGE(x) curve')
+    #ax[0,1].plot(np.linspace(0, 10, vis_len, endpoint=False), In_data[2], 'r-', label="Gauss no. 2")
+    #ax[0,1].plot(np.linspace(0, 10, vis_len, endpoint=False), In_data[3], 'b-', label="Gauss no. 3")
 
     #ax[0,0].plot(Y_NN[1][0], Gauss(Y_NN[1][0], 1, Y_NN[1][0], Y_NN[1][1]), 'k*', label="Predicted mean")
     #ax[0,0].plot(Y_NN[2][0], Gauss(Y_NN[2][0], 1, Y_NN[2][0], Y_NN[2][1]), 'r*', label="Predicted mean")

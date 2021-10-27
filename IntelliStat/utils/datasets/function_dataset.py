8a6768df616fd65f3cc8b09e64306bcc448da67f
MichalKacprzak99 <michal.kacprzak999@gmail.com>
2021-10-27 23:32:52 +0200

    simplify FunctionDataset logic

--- IntelliStat/utils/datasets/function_dataset.py
+++ IntelliStat/utils/datasets/function_dataset.py
@@ -8,9 +8,7 @@ from .base_dataset import BaseDataset
 class FunctionDataset(BaseDataset):
     def __init__(self, X, function: str, params: Union[np.ndarray, Sequence]):
 
-        Y = np.zeros(X.shape[0])
         if function == "linear":
-            Y = [[self.X[point][0] * params[0] + params[1]] for point in range(X.shape[0])]
-        Y = np.array(Y, dtype=np.float32)
+            Y = X * params[0] + params[1]
 
         super().__init__(X, Y)

14e7e971f0d4d494bc7007e9e112dee4e3c31444
MichalKacprzak99 <michal.kacprzak999@gmail.com>
2021-11-04 23:16:09 +0100

    delete redundant functions

--- IntelliStat/generic_builders/shape_builder/component_functions.py
+++ IntelliStat/generic_builders/shape_builder/component_functions.py
@@ -17,31 +17,8 @@ def GG(x: np.ndarray, A1: float = 0.7 + 0.6 * random(), u1: float = 1 + 8 * rand
     return Gauss(x, A1, u1, sigma1) + Gauss(x, A2, u2, sigma2)
 
 
-def GGG(x: np.ndarray, A1: float = 0.7 + 0.6 * random(), u1: float = 1 + 8 * random(),
-        sigma1: float = 0.1 + 0.4 * random(), A2: float = 0.7 + 0.6 * random(), u2: float = 1 + 8 * random(),
-        sigma2: float = 0.1 + 0.4 * random(), A3: float = 0.7 + 0.6 * random(), u3: float = 1 + 8 * random(),
-        sigma3: float = 0.1 + 0.4 * random()) -> np.ndarray:
-    return Gauss(x, A1, u1, sigma1) + Gauss(x, A2, u2, sigma2) + Gauss(x, A3, u3, sigma3)
-
-
 def GGE(x: np.ndarray, A1: float = 0.7 + 0.6 * random(), u1: float = 1 + 8 * random(),
         sigma1: float = 0.1 + 0.4 * random(), A2: float = 0.7 + 0.6 * random(),
         u2: float = 1 + 8 * random(), sigma2: float = 0.1 + 0.4 * random(), B1: float = 0.7 + 0.6 * random(),
         b1: float = 0.5 + random()) -> np.ndarray:
     return Gauss(x, A1, u1, sigma1) + Gauss(x, A2, u2, sigma2) + Exp(x, B1, b1)
-
-
-def GE(x: np.ndarray, A1: float = 0.7 + 0.6 * random(), u1: float = 1 + 8 * random(),
-       sigma1: float = 0.1 + 0.4 * random(), B1: float = 0.7 + 0.6 * random(),
-       b1: float = 0.5 + random()) -> np.ndarray:
-    return Gauss(x, A1, u1, sigma1) + Exp(x, B1, b1)
-
-
-def multiG(x: np.ndarray, n: int = 0) -> np.ndarray:
-    multi_g: np.ndarray = np.zeros(x.shape)
-    for it in range(n):
-        u = 1 + 8 * random()
-        A = 0.7 + 0.6 * random()
-        sigma = 0.2 + 0.5 * random()
-        multi_g += Gauss(x, A, u, sigma)
-    return multi_g

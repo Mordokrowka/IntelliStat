5797a166601ad7f3bada341c6ee3975824fc38d7
MichalKacprzak99 <michal.kacprzak999@gmail.com>
2021-11-07 22:31:26 +0100

    ads typing

--- IntelliStat/generic_builders/base_builder.py
+++ IntelliStat/generic_builders/base_builder.py
@@ -1,24 +1,24 @@
 import json
 from functools import singledispatch
 from pathlib import Path
-from typing import Optional
+from typing import List, Optional
 from types import SimpleNamespace
 
 from jsonschema import validate
 
 
 @singledispatch
-def wrap_namespace(ob):
+def wrap_namespace(ob) -> SimpleNamespace:
     return ob
 
 
 @wrap_namespace.register(dict)
-def _wrap_dict(ob):
+def _wrap_dict(ob) -> SimpleNamespace:
     return SimpleNamespace(**{k: wrap_namespace(v) for k, v in ob.items()})
 
 
 @wrap_namespace.register(list)
-def _wrap_list(ob):
+def _wrap_list(ob) -> List[SimpleNamespace]:
     return [wrap_namespace(v) for v in ob]
 
 
@@ -28,13 +28,13 @@ class BaseBuilder:
         self.config_schema_file: Optional[Path] = None
 
     @staticmethod
-    def load_config_schema(config_schema_file: Optional[Path] = None):
-        with config_schema_file.open() as fp:
+    def load_config_schema(config_schema_file: Path):
+        with open(config_schema_file) as fp:
             return json.load(fp)
 
-    def load_configuration(self, config_file: Path, config_schema_file: Optional[Path] = None):
+    def load_configuration(self, config_file: Path, config_schema_file: Optional[Path] = None) -> SimpleNamespace:
         config_schema_file = config_schema_file if config_schema_file else self.config_schema_file
-        with config_file.open() as fp:
+        with open(config_file) as fp:
             config_data = json.load(fp)
             validate(config_data, schema=self.load_config_schema(config_schema_file))
             return wrap_namespace(config_data)

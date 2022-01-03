98a378219c32c1957a0e1c182f9a43ab4fb14959
MichalKacprzak99 <michal.kacprzak999@gmail.com>
2022-01-03 23:06:16 +0100

    adjust arguments name

--- IntelliStat/neural_networks/enn_classifier.py
+++ IntelliStat/neural_networks/enn_classifier.py
@@ -23,13 +23,13 @@ class ENN_Classifier(BaseNeuralNetwork):
         self.optimizer = Adam(self.model.parameters(), lr=learning_rate)
         self.loss_vector = []
 
-    def train(self, data: Dataset, epochs: int, batch: Optional[int]):
-        data_train = DataLoader(dataset=data, batch_size=batch, shuffle=True)
+    def train(self, dataset: Dataset, epochs: int, batch: Optional[int]):
+        data_train = DataLoader(dataset=dataset, batch_size=batch, shuffle=True)
         criterion = torch.nn.CrossEntropyLoss()
         for epoch in range(epochs):
             if epoch % 100 == 0:
                 print("Training epoch: ", epoch)
-            for dummy, batch in enumerate(data_train):
+            for batch in data_train:
                 x_train, y_train = batch['input'], batch['output']
                 self.train_step(x_train, y_train, criterion)
             loss = criterion(self.model(x_train.float()), y_train)
